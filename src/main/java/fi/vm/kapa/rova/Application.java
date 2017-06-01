6c6ee23efa17c21a256a14a6f11cc898bff39d6e
Juha Korkalainen <juha.korkalainen@iki.fi>
2017-06-01 14:19:03 +0300

    fixed deprecated import

--- src/main/java/fi/vm/kapa/rova/Application.java
+++ src/main/java/fi/vm/kapa/rova/Application.java
@@ -24,7 +24,7 @@ package fi.vm.kapa.rova;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
